f895b580a6eaa775d755e55d181a09f660f67521
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-01-15 00:06:30 +0200

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,22 +5,25 @@ import { decrypt } from "./app/lib/session";
 const protectedRoutes = ["/"];
 const publicRoutes = ["/login"];
 
-
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoutes = publicRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
 
   const cookie = (await cookies()).get("session")?.value;
   const session = await decrypt(cookie);
 
   if (isProtectedRoute && !session?.userId) {
-    return NextResponse.redirect("/login");
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
-  if (isPublicRoutes && !session?.userId) {
-    return NextResponse.redirect("/");
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
   return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
